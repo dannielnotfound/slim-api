73da0a23f5d524c5806a56e6cbea08e51373e6ee
dannielnotfound <danielriber548@gmail.com>
2022-07-07 17:35:27 -0300

    .

--- public/index.php
+++ public/index.php
@@ -13,6 +13,27 @@ $app->get('/', function(Request $request, Response $response, $args){
     return $response;
 });
 
+$app->get('/books', function(Request $request, Response $response, $args){
+   $books = [
+    '1' => 'Book 1',
+    '2' => 'Book 2',
+    '3' => 'Book 3',
+   ];
+   $response->getBody()->write(json_encode($books));
+   return $response->withHeader('Content-type', 'application/json');
+});
+
+$app->get('/books/{id}', function(Request $request, Response $response, $args){
+    $books = [
+        '1' => 'Book 1',
+        '2' => 'Book 2',
+        '3' => 'Book 3',
+    ];
+    $book[$args['id']] =  $books[$args['id']];
+    $response->getBody()->write(json_encode($book));
+    return $response->withHeader('Content-type', 'application/json');
+});
+
 $app->run();
 
 ?>
